2015627b1d526c773743cb10519ca78afd805bff
Darshinikannan <86762304+Darshinikannan@users.noreply.github.com>
2022-11-17 00:55:21 +0530

    Update TestRunner.java

--- src/test/java/com/cucumber/TestRunner.java
+++ src/test/java/com/cucumber/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/rds.feature", 
 glue = {"com.cucumber"}, 
-plugin = {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+plugin = {"json:target/cucumber.json"}},
 publish = true,
 monochrome = true, 
 dryRun = false)
